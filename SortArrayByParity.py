bc2705d1e6e294855f1af46d63e63f9b27d212fd
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-26 09:14:30 +0530

    completed sort array by parity

--- SortArrayByParity.py
+++ SortArrayByParity.py
@@ -6,6 +6,11 @@ def sortArrayByParity(arr):
         if arr[i] % 2 != 0:
             print(arr[i], end = ' ')
 
+def sortArray(arr):
+    arr.sort(key = lambda x: x % 2)
+    return arr
 
 arr = [int(x) for x in input().split()]
-sortArrayByParity(arr)
+#sortArrayByParity(arr)
+k = sortArray(arr)
+print(k)
