541dfa9fd7634d8e6acd2bacd29285a98076cdf2
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-28 07:21:15 +0530

    done with non decreasing array

--- NonDecreasingArr.py
+++ NonDecreasingArr.py
@@ -1,16 +1,30 @@
+#665
+
 def NonDecreasingArray(arr):
     count = 0
     for i in range(len(arr) - 1):
+        
         if arr[i] > arr[i + 1]:
             count += 1
+            
             if count >= 2:
                 return False
+                
             elif (i - 1) >= 0 and arr[i + 1] < arr[i - 1]:
+                
+                #print("cha;a")
                 arr[i + 1] = arr[i]
+                #print(arr)
+              
     return True
-
+    
     
 
 arr = [int(x) for x in input().split()]
 k = NonDecreasingArray(arr)
-print(k)
+if(k):
+    print("true")
+else:
+    print("false")
+
+#ex=3 4 2 3, ex 3 4 2
