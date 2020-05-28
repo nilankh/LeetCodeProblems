11c07aa72c5d41d3164f2f7e28922ba577eaea6a
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-28 08:47:40 +0530

    done search in rotate array

---
+++ searchInRotatedArray.py
@@ -0,0 +1,21 @@
+def searchInRotate(arr):
+    #return target in arr
+    if target in arr:
+        return True
+    return False
+
+    
+#sahi h
+##    for i in arr:
+##        if i == target:
+##            return True
+##    else:
+##        return False
+
+
+arr = [int(x) for x in input().split()]
+target = int(input())
+if(searchInRotate(arr)):
+    print("true")
+else:
+    print("false")
