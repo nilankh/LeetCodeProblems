42ec6524a5bdf8480a65352553ba70e5cc10fbf1
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-30 20:48:29 +0530

    create linked list folder and solved one question even odd

---
+++ LinkedList/evenodd.py
@@ -0,0 +1,62 @@
+#328
+
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def printLL(head):
+    while head is not None:
+        print(str(head.data) + "->", end = "")
+        head = head.next
+    print()
+    return
+
+def takeInput():
+    inputList = [int(ele) for ele in input().split()]
+    head = None
+    tail = None
+
+    for currData in inputList:
+        if currData == -1:
+            break
+        newNode = Node(currData)
+
+        if head is None:
+            head = newNode
+            tail = newNode
+        else:
+            tail.next = newNode
+            tail = newNode
+    return head
+def oddEven(head):
+    if not head or not head.next:
+            return head
+        
+    odd = head
+    even = head.next
+    while odd.next and odd.next.next:
+        temp = odd.next
+        odd.next = odd.next.next
+        odd = odd.next
+        temp.next = odd.next
+    odd.next = even
+    return head
+    
+   
+head = takeInput()
+printLL(head)
+
+
+printLL(oddEven(head))
+
+
+
+
+
+
+
+
+
