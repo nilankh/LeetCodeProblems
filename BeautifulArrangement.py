9f0d6dce9c971f2b764bd9c72d280cfd5e476d0c
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-06-01 13:07:15 +0530

    applied backtracking

--- BeautifulArrangement.py
+++ BeautifulArrangement.py
@@ -1,91 +1,129 @@
 #526
-'''
-Suppose you have N integers from 1 to N. We define a beautiful arrangement as an array that is constructed by these N numbers successfully if one of the following is true for the ith position (1 <= i <= N) in this array:
-
-The number at the ith position is divisible by i.
-i is divisible by the number at the ith position.
- 
-
-Now given N, how many beautiful arrangements can you construct?
-
-Example 1:
-
-Input: 2
-Output: 2
-Explanation: 
-
-The first beautiful arrangement is [1, 2]:
-
-Number at the 1st position (i=1) is 1, and 1 is divisible by i (i=1).
-
-Number at the 2nd position (i=2) is 2, and 2 is divisible by i (i=2).
-
-The second beautiful arrangement is [2, 1]:
-
-Number at the 1st position (i=1) is 2, and 2 is divisible by i (i=1).
-
-Number at the 2nd position (i=2) is 1, and i (i=2) is divisible by 1.
- 
-
-Note:
-
-N is a positive integer and will not exceed 15.'''
-
 
 
 #BRUTEFORCE
 
+##count = 0
+##def countArrangement(n):
+##
+##    global count
+##    
+##    arr = [0] * n
+##    for i in range(1, n + 1):
+##        arr[i - 1] = i
+##    
+##    permute(arr, 0)
+##    
+##    return count
+##        
+##    
+##def permute(arr, l):
+##    global count
+##    if(l == len(arr) - 1):
+##        i = 1
+##        while(i <= len(arr)):
+##            if arr[i - 1] % i != 0 and i % arr[i - 1] != 0:
+##                break
+##            i+= 1        
+##        if(i == len(arr) + 1):
+##            count += 1
+##            
+##    for i in range(l, len(arr)):
+##        
+##        swap(arr, i, l)
+##        permute(arr, l + 1)
+##        swap(arr, i, l)
+##
+##def swap(arr, x, y):
+##    temp = arr[x]
+##    arr[x] = arr[y]
+##    arr[y] = temp
+##    
+
+##from sys import setrecursionlimit
+##setrecursionlimit(10000)
+##n = int(input())
+##
+##k = countArrangement(n)
+##print(k)
+
+
+
+#BRUTE FORCE ACCEPTED
+
+##count = 0
+##def countArrangementA(n):
+##
+##    global count
+##    
+##    arr = [0] * n
+##    for i in range(1, n + 1):
+##        arr[i - 1] = i
+##    
+##    permuteA(arr, 0)
+##    
+##    return count
+##        
+##    
+##def permuteA(arr, l):
+##    global count
+##    if(l == len(arr)):
+##        count += 1
+##    for i in range(l, len(arr)):
+##        
+##        swapA(arr, i, l)
+##        if(arr[l] %(l + 1) == 0 or (l + 1) % arr[l] == 0):
+##            permuteA(arr, l + 1)
+##        swapA(arr, i, l)
+##  
+##
+##def swapA(arr, x, y):
+##    temp = arr[x]
+##    arr[x] = arr[y]
+##    arr[y] = temp
+##    
+##
+##from sys import setrecursionlimit
+##setrecursionlimit(10000)
+##n = int(input())
+##
+##k = countArrangementA(n)
+##print(k)
+
+#BackTracKing
+
 count = 0
 def countArrangement(n):
-
     global count
-    
-    arr = [0] * n
-    for i in range(1, n + 1):
-        arr[i - 1] = i
-    
-    permute(arr, 0)
-    
+    visited = [False for i in range(n + 1)]
+    calculate(n, 1, visited)
     return count
-        
-    
-def permute(arr, l):
+
+def calculate(n, pos, visited):
     global count
-##    count += 1
-##    print('ksjfskd', count)
-    if(l == len(arr) - 1):
-        i = 1
-        while(i <= len(arr)):
-            if arr[i - 1] % i != 0 and i % arr[i - 1] != 0:
-                break
-            i+= 1
-
-##        i = 1
-##        for i in range(1, len(arr) + 1):
-##            
-        if(i == len(arr) + 1):
-            count += 1
+    if(pos > n):
+        count += 1
+    for i in range(1, n + 1):
+        if(visited[i] is False and (pos % i == 0 or i % pos == 0)):
+            visited[i] = True
+            calculate(n, pos + 1, visited)
+            visited[i] = False
             
-
-    #print(count)
-    for i in range(l, len(arr)):
-        
-        swap(arr, i, l)
-        permute(arr, l + 1)
-        swap(arr, i, l)
-
-def swap(arr, x, y):
-    temp = arr[x]
-    arr[x] = arr[y]
-    arr[y] = temp
-    
-
-from sys import setrecursionlimit
-setrecursionlimit(10000)
 n = int(input())
-
 k = countArrangement(n)
 print(k)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
