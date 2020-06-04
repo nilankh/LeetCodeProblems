e29787b8e2751625d64bc4221c17788eb942ab3d
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-06-04 11:24:07 +0530

    added 1 question in 30 day's challenge

---
+++ 30daysleetcodechallenge/TwoCityscheduling.py
@@ -0,0 +1,65 @@
+#1029
+'''
+There are 2N people a company is planning to interview. The cost of flying the i-th person to city A is costs[i][0], and the cost of flying the i-th person to city B is costs[i][1].
+
+Return the minimum cost to fly every person to a city such that exactly N people arrive in each city.
+
+ 
+
+Example 1:
+
+Input: [[10,20],[30,200],[400,50],[30,20]]
+Output: 110
+Explanation: 
+The first person goes to city A for a cost of 10.
+The second person goes to city A for a cost of 30.
+The third person goes to city B for a cost of 50.
+The fourth person goes to city B for a cost of 20.
+
+The total minimum cost is 10 + 30 + 50 + 20 = 110 to have half the people interviewing in each city.
+ 
+
+Note:
+
+1 <= costs.length <= 100
+It is guaranteed that costs.length is even.
+1 <= costs[i][0], costs[i][1] <= 1000'''
+
+def twoCity(cost):
+##    k = sorted(cost, key = lambda x:x[0]-x[1])
+##    print(k)
+    refund = []
+    n = len(cost) // 2
+    minCost = 0
+    for a, b in cost:
+        #print("a", a , "    ", "b", b)
+        refund.append(b - a)
+        minCost += a
+    refund.sort()
+    #print(refund)
+    for i in range(n):
+        minCost += refund[i]
+    return minCost
+
+
+str = input().split()
+n, m = int(str[0]), int(str[1])
+b = str[2:]
+cost= [[int(b[m * i + j]) for j in range(m)] for i in range(n)]
+
+print(twoCity(cost))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
