32aafd77c1bab2a4eb98aa58a21077daf26f0265
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-06-08 07:41:57 +0530

    recursion

--- 30daysleetcodechallenge/coinchange.py
+++ 30daysleetcodechallenge/coinchange.py
@@ -5,11 +5,40 @@ def coinChange(amount, coins):
     for c in coins:
         for j in range(c, amount + 1):
             dp[j] += dp[j - c]
+        print(dp)
     return dp[amount]
 
 
 
 
+##amount = int(input())
+##coins = [int(x) for x in input().split()]
+##print(coinChange(amount, coins))
+
+def coinChangeRR(amount, coins):
+    return helper(amount, coins, 0)#0 index h
+
+def helper(amount, coins, index):
+    if amount == 0:
+        return 1
+    if amount < 0 or index == len(coins):
+        return 0
+    result = 0
+    for i in range(index, len(coins)):
+        if amount >= coins[i]:
+            result += helper(amount - coins[i], coins, i)
+
+    return result
 amount = int(input())
 coins = [int(x) for x in input().split()]
-print(coinChange(amount, coins))
+print(coinChangeRR(amount, coins))
+
+
+
+
+
+
+
+
+
+
