828aef098fe74801ca262a20db1ce173f7caf4f8
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-06-12 10:30:24 +0530

    done with tle error

--- LinkedList/NextGreaterNodeInLL1019.py
+++ LinkedList/NextGreaterNodeInLL1019.py
@@ -34,32 +34,44 @@ def takeInput():
 
 #the code is fully correct but it will not work
 #longer inputs so tle error will show
+##def nextLargerNodes(head):
+##    curr = head
+##    Next = curr.next
+##    arr = []
+##    while curr != None:
+##        while Next is not None:
+##            if curr.data < Next.data:
+##                arr.append(Next.data)
+##                curr = curr.next
+##                if curr.next is not None:
+##                    
+##                    Next = curr.next
+##                else:
+##                    break
+##            else:
+##                Next = Next.next
+##        arr.append(0)
+##        if curr.next is not None:
+##            curr = curr.next
+##        
+##            Next = curr.next
+##        else:
+##            break
+##        
+##    return arr
 def nextLargerNodes(head):
-    curr = head
-    Next = curr.next
     arr = []
-    while curr != None:
-        while Next is not None:
-            if curr.data < Next.data:
-                arr.append(Next.data)
-                curr = curr.next
-                if curr.next is not None:
-                    
-                    Next = curr.next
-                else:
-                    break
-            else:
-                Next = Next.next
-        arr.append(0)
-        if curr.next is not None:
-            curr = curr.next
-        
-            Next = curr.next
-        else:
-            break
-        
-    return arr
-
+    current = head
+    while current:
+        arr.append(current.data)
+        current = current.next
+    stack = []
+    result = [0] * len(arr)
+    for i in range(len(arr)):
+        while stack and (arr[stack[len(stack) - 1]] < arr[i]):
+            result[stack.pop()] = arr[i]
+        stack.append(i)
+    return result
 head = takeInput()
 #printLL(head)
 k = nextLargerNodes(head)
