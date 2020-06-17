4618d26d30db278a0a666a399317dbdb51e5e8e9
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-06-17 21:59:58 +0530

    done with countminimum bracket reversal

--- stack/MinimumBracketReversal.py
+++ stack/MinimumBracketReversal.py
@@ -4,7 +4,25 @@ def countBracketReversals(string):
         return 0
     if len(string) % 2 != 0:
         return -1
-    
+    s = []
+    for char in string:
+        if char == '{':
+            s.append(char)
+        else:
+            if len(s) > 0 and s[-1] == '{':
+                s.pop()
+            else:
+                s.append(char)
+    count = 0
+    while(len(s) != 0):
+        c1 = s.pop()
+        c2 = s.pop()
+        if c1 != c2:
+            count += 2
+        else:
+            count += 1
+    return count
+            
 
 
 
@@ -12,3 +30,15 @@ def countBracketReversals(string):
 string = input()
 ans = countBracketReversals(string)
 print(ans)
+
+
+
+
+
+
+
+
+
+
+
+
