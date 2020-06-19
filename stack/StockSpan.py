92edab5a7ec8802d9726850a647c6acd2e13d9e2
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-06-19 12:17:04 +0530

    done with stockspan method2

--- stack/StockSpan.py
+++ stack/StockSpan.py
@@ -7,9 +7,23 @@ def stockSpan(price,n):
             c=c+ans[i-c]
         ans.append(c)
     return ans
+def stockSpan2(price, n):
+    s = []
+    output = []
+    s.append(0)
+    output.append(1)
 
+    for i in range(1, len(price)):
+        while(len(s) != 0 and price[s[-1]] < price[i]):
+            s.pop()
+        if len(s) == 0:
+            output.append(i + 1)
+        else:
+            output.append(i - s[-1])
+        s.append(i)
+    return output
 n=int(input())
 price=[int(ele) for ele in input().split()]
-spans=stockSpan(price,n)
+spans=stockSpan2(price,n)
 for ele in spans:
     print(ele,end=' ')
