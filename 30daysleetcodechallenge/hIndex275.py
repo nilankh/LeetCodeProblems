42b15bfc1322994233a064966147d5c0d81ecb03
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-06-19 08:31:29 +0530

    done with second method

--- 30daysleetcodechallenge/hIndex275.py
+++ 30daysleetcodechallenge/hIndex275.py
@@ -1,5 +1,6 @@
 #275
-def Hindex(arr):
+def Hindex1(arr):
+    #timecomplexity is O(n)
     n = len(citations)
     i = n - 1
     while(i >= 0):
@@ -7,8 +8,22 @@ def Hindex(arr):
             break
         i -= 1
     return n - i - 1
+
+
+def Hindex2(citations):
+    n = len(citations)
+    l = 0
+    r = n - 1
+    while(l <= r):
+        mid = l + (r - l)//2
+        if(citations[mid] < n - mid):
+            l = mid + 1
+        else:
+            r = mid - 1
+    return n - l
     
 
 
 citations = [int(x) for x in input().split()]
-print(Hindex(citations))
+#print(Hindex1(citations))
+print(Hindex2(citations))
