ba5b45be7730af240dfac1ecf6407d0c6d167b0a
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-06-25 11:24:39 +0530

    done with reverse firt k elements in queue

--- Queue/ReverseFirstKEle.py
+++ Queue/ReverseFirstKEle.py
@@ -1,11 +1,20 @@
 import queue
 
+def reverseQueue(q, k):
+    if k <= 0 or q.qsize() <= 1 :
+        return
+    ele = q.get()
+    reverseQueue(q, k - 1)
+    q.put(ele)
+    return
 
 def reverseFirstK(q, k):
     reverseQueue(q, k)
     n = q.qsize()
     for i in range(n - k):
         q.put(q.get())
+
+        
 from sys import setrecursionlimit
 setrecursionlimit(11000)
 n = int(input())
@@ -19,3 +28,25 @@ while(q.qsize() > 0):
     print(q.get())
     n -= 1
     
+
+##import queue
+##q = queue.Queue()
+##q.put(1)
+##q.put(2)
+##q.put(3)
+##q.put(4)
+##q.put(5)
+##
+##while(q.qsize()>0):
+##    print(q.get())
+##print(q.qsize())
+
+
+
+
+
+
+
+
+
+
