009a3658c3a21982fbb6282548b273c4e8fe29f2
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-06-25 11:16:03 +0530

    done reverse  function of reverse first k ele in queue

--- Queue/ReverseFirstKEle.py
+++ Queue/ReverseFirstKEle.py
@@ -1,5 +1,11 @@
 import queue
 
+
+def reverseFirstK(q, k):
+    reverseQueue(q, k)
+    n = q.qsize()
+    for i in range(n - k):
+        q.put(q.get())
 from sys import setrecursionlimit
 setrecursionlimit(11000)
 n = int(input())
@@ -8,6 +14,7 @@ q = queue.Queue()
 for ele in li:
     q.put(ele)
 k = int(input())
+reverseFirstK(q, k)
 while(q.qsize() > 0):
     print(q.get())
     n -= 1
