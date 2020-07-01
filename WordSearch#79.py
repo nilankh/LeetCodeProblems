9ac1ca09d1c8a82bb4af8fed102b07cefe3ff1c7
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-01 21:45:01 +0530

    done with #79

---
+++ WordSearch#79.py
@@ -0,0 +1,22 @@
+#79
+def exist(board, word):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == word[0] and dfs(board, i, j, 0, word):
+                return True
+    return False
+def dfs(board, i, j, count, word):
+    if count == len(word):
+        return True
+    if(i == -1 or i == len(board) or j == -1 or j == len(board[0]) or board[i][j] != word[count]):
+        return False
+    temp = board[i][j]
+    board[i][j] = '*'
+    found = dfs(board, i + 1, j, count + 1, word) or dfs(board, i - 1, j, count + 1, word) or dfs(board, i, j + 1, count + 1, word) or dfs(board, i, j - 1, count + 1, word)
+    board[i][j] = temp
+    return found
+
+board =[['A','N','S','Q'],['S','O','L','R'],['K','T','O','G']]
+
+word = "SOLE"
+print(exist(board, word))
