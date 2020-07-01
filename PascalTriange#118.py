f6c316d68d659a3cb4b8b7abf0af131722bc179b
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-01 09:36:41 +0530

    #119

--- PascalTriange#118.py
+++ PascalTriange#118.py
@@ -4,13 +4,15 @@ def pascalTriangle(n):
     for i in range(1, n + 1):
         l = []
         for j in range(1, i + 1):
+            #begining and ending values
             if j == 1 or j == i:
                 l.append(1)
             else:
+                #and this is between values(u can refers amulya academy)
                 l.append(res[-1][j - i - 1] + res[-1][j - i])
         res.append(l)
     return res
-    
+
 
 n = int(input())
 k = pascalTriangle(n)
