e24626f3c7da829c81a5bd3177e08a24651d4494
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-05 13:27:49 +0530

    done

---
+++ Recursion/PrintMinimumOFArray.py
@@ -0,0 +1,7 @@
+def printMin(l, minSoFar = 100000000):
+    if len(l) == 0:
+        print(minSoFar)
+        return
+    newMin = min(minSoFar, l[0])
+    printMin(l[1:], newMin)
+printMin([1,2,3,4,5,-1,0,-2,-4,-5,6])
