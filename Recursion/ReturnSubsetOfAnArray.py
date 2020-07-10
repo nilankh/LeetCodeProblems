bdd6b5c38469d2accdc321c1085245839fd72b1f
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-10 08:30:58 +0530

    working on retuurn subssets input part done

--- Recursion/ReturnSubsetOfAnArray.py
+++ Recursion/ReturnSubsetOfAnArray.py
@@ -1,4 +1,10 @@
-
+def SubsetArray(arr):
+    
 
 n = int(input())
 arr = [int(c) for c in input().split()]
+output = SubsetArray(arr)
+for lst in output:
+    for num in lst:
+        print(num, end = ' ')
+    print()
