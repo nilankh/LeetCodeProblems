9d0521d33cd6e70b83e88d8f9d2327366bc85edb
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-12 09:28:54 +0530

    done with permutation of string

---
+++ Recursion/PrintPermutaionOFString.py
@@ -0,0 +1,32 @@
+def permutation(inputS, result, count, level):
+    
+    if(level == len(inputS)):
+        #print the result
+        for i in range(len(result)):
+            print(result[i], end = "")
+        print()
+        return
+    else:
+        for i in range(len(inputS)):
+            if count[i] == 0:
+                continue
+            else:
+                result[level] = inputS[i]
+                count[i] -= 1
+                permutation(inputS, result, count, level + 1)
+                count[i] += 1
+
+
+
+inputS = input()
+result = [0 for i in range(len(inputS))]
+count = [1 for i in range(len(inputS))]
+permutation(inputS, result, count, 0)
+
+
+
+
+
+
+
+
