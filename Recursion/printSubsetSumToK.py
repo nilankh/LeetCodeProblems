add9972cec8d4d1202f416252bba0b66701e10ba
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-14 11:41:05 +0530

    done with print subset of an array with sum = k

--- Recursion/printSubsetSumToK.py
+++ Recursion/printSubsetSumToK.py
@@ -1,2 +1,20 @@
 #rough23
+def printSubsetHelper(arr, k, lst):
+    n = len(arr)
+    if n == 0:
+        return
+    if n == 1:
+        if arr[0] == k:
+            print(k, *lst)
+            return
+    printSubsetHelper(arr[:-1], k, lst)
+    printSubsetHelper(arr[:-1], k - arr[-1], [arr[-1]]+lst)
+    if arr[-1] == k:
+        print(k, *lst)
 
+def printSubset(arr, k):
+    printSubsetHelper(arr, k, [])
+n = int(input())
+arr = [int(z) for z in input().split()]
+k = int(input())
+printSubset(arr, k)
