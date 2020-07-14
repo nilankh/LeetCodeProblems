4d41242536a10d87f7f89e0f1fc859278e1c888f
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-14 22:25:17 +0530

    learning about regualr expressions

--- RegularExpression/RegExLearning1.py
+++ RegularExpression/RegExLearning1.py
@@ -1,8 +1,12 @@
 import re
+##txt = "The rain in Spain"
+##x = re.search("^The.*Spain$", txt)
+####print(x)
+##if x:
+##    print("YES! We have a match!")
+##else:
+##    print("No Match")
+
 txt = "The rain in Spain"
-x = re.search("^The.*Spain$", txt)
-##print(x)
-if x:
-    print("YES! We have a match!")
-else:
-    print("No Match")
+x = re.findall("ai", txt)
+print(x)
