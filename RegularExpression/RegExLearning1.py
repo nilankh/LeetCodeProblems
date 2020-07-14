ad3c6e702349d662fe927249b013c6b5bc11f81e
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-14 22:28:13 +0530

    learning about regualr expressions

--- RegularExpression/RegExLearning1.py
+++ RegularExpression/RegExLearning1.py
@@ -17,6 +17,46 @@ import re
 ##
 ##If no matches are found, an empty list is returned:
 
+##txt = "The rain in Spain"
+##x = re.findall("Portugal", txt)
+##print(x)
+
+
+
+
+##The search() Function
+##The search() function searches the string for a match, and returns a Match object if there is a match.
+##
+##If there is more than one match, only the first occurrence of the match will be returned:
+
 txt = "The rain in Spain"
-x = re.findall("Portugal", txt)
-print(x)
+x = re.search("\s", txt)
+
+print("The first white-space character is located in position:", x.start())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
