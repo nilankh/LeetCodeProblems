ead154dacc12a96a39b659db426e663f6820fa16
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-14 22:29:10 +0530

    learning about regualr expressions

--- RegularExpression/RegExLearning1.py
+++ RegularExpression/RegExLearning1.py
@@ -29,12 +29,14 @@ import re
 ##
 ##If there is more than one match, only the first occurrence of the match will be returned:
 
-txt = "The rain in Spain"
-x = re.search("\s", txt)
-
-print("The first white-space character is located in position:", x.start())
-
-
+##txt = "The rain in Spain"
+##x = re.search("\s", txt)
+##
+##print("The first white-space character is located in position:", x.start())
+##If no matches are found, the value None is returned:
+##txt = "The rain in Spain"
+##x = re.search("Portugal", txt)
+##print(x)
 
 
 
