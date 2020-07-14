f967db48537a21cb021cb00b8eb1dbc3a649142d
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-14 22:30:39 +0530

    learning about regualr expressions

--- RegularExpression/RegExLearning1.py
+++ RegularExpression/RegExLearning1.py
@@ -43,12 +43,14 @@ import re
 ##The split() Function
 ##The split() function returns a list where the string has been split at each match:
 
-txt = "The rain in Spain"
-x = re.split("\s", txt)
-print(x)
+##txt = "The rain in Spain"
+##x = re.split("\s", txt)
+##print(x)
 
-
-
+##You can control the number of occurrences by specifying the maxsplit parameter:
+##txt = "The rain in Spain"
+##x = re.split("\s", txt, 1)
+##print(x)
 
 
 
