d68ee43914d5c7a9de12d512a45a9153ca68bc7b
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-14 22:26:18 +0530

    learning about regualr expressions

--- RegularExpression/RegExLearning1.py
+++ RegularExpression/RegExLearning1.py
@@ -7,6 +7,16 @@ import re
 ##else:
 ##    print("No Match")
 
+
+##The findall() Function
+##The findall() function returns a list containing all matches.
+##txt = "The rain in Spain"
+##x = re.findall("ai", txt)
+##print(x)
+##The list contains the matches in the order they are found.
+##
+##If no matches are found, an empty list is returned:
+
 txt = "The rain in Spain"
-x = re.findall("ai", txt)
+x = re.findall("Portugal", txt)
 print(x)
