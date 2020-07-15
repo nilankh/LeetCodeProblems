4b4aac1ffc15d47d9e3520de76855c72808478f2
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-15 08:14:08 +0530

    done with seconde method

--- JULY30DAYSLEETCODECHALLENGE/ArrangingCoins#441.py
+++ JULY30DAYSLEETCODECHALLENGE/ArrangingCoins#441.py
@@ -1,15 +1,31 @@
 #441
+##FIRST METHOD
+##def arrangeCoins(n):
+##    if n < 2:
+##        return n
+##    else:
+##        i = 1
+##        count = 0
+##        while count <= n:
+##            count += i
+##            i += 1
+##        return i - 2
 
+#2ND METHOD
 def arrangeCoins(n):
-    if n < 2:
-        return n
-    else:
-        i = 1
-        count = 0
-        while count <= n:
-            count += i
-            i += 1
-        return i - 2
+    left = 0
+    right = n
+    while left <= right:
+        k = (right + left)//2
+        curr = k * (k + 1)//2
+        if curr == n:
+            return k
+        if n < curr:
+            right = k - 1
+        else:
+            left = k + 1
+    return right
+
 
 n = int(input())
 print(arrangeCoins(n))
