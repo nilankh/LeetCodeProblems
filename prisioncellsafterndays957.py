a71936fc70cf73a11d5e07c893eab20f5a812f29
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-17 09:27:18 +0530

    done 957

---
+++ prisioncellsafterndays957.py
@@ -0,0 +1,24 @@
+#957
+def prisonAfterNDays(cells, N):
+        _map = {}
+    
+        for i in range(N):
+            s = str(cells)
+            if s in _map:
+                loop_length = i - _map[s]
+                remaining_days = (N - i) % loop_length
+                return prisonAfterNDays(cells, remaining_days)
+            else:
+                _map[s] = i
+                prev = cells[0]
+                for j in range(1,7):
+                    curr, next = cells[j], cells[j+1]
+                    cells[j] = 1 - (prev ^ next) #!(prev ^ next)
+                    prev = curr
+            cells[0], cells[7] = 0,0
+        
+        return cells
+
+arr = [int(x) for x in input().split()]
+N = int(input())
+print(prisonAfterNDays(arr,N))
