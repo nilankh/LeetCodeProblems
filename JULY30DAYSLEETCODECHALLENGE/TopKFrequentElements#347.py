2220b8c2f2caec808ea42c7b52fd4d39f096720b
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-18 15:53:25 +0530

    done

---
+++ JULY30DAYSLEETCODECHALLENGE/TopKFrequentElements#347.py
@@ -0,0 +1,43 @@
+#347
+#rough pageno 59
+
+def topKfreqElements(arr, k):
+    arr.sort()
+    d = {}
+    
+    for w in arr:
+        if w in d:
+            d[w] += 1
+        else:
+            d[w] = 1
+    print(d.sort())
+    l = []
+    for i in d:
+        if d[i] >= k:
+            l.append(i)
+        if len(l) == 0:
+            return l
+    return l
+
+
+
+
+arr = [int(x) for x in input().split()]
+k = int(input())
+f = topKfreqElements(arr, k)
+print(f)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
