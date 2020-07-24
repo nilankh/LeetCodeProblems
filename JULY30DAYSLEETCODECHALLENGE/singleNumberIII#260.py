093e3701069929598e4d9df2476883724fc1e247
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-24 19:08:29 +0530

    done

---
+++ JULY30DAYSLEETCODECHALLENGE/singleNumberIII#260.py
@@ -0,0 +1,18 @@
+#260
+def singleNumber(arr):
+    d = {}
+    for w in arr:
+        if w in d:
+            d[w] += 1
+        else:
+            d[w] = 1
+    k = []
+    for i in d:
+        if d[i] == 1:
+            k.append(i)
+    return k
+            
+
+arr = [int(x) for x in input().split()]
+k = singleNumber(arr)
+print(*k)
