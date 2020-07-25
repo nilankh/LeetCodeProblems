c7c853fcf810eddf0d3942d0b47427b5f48b8dc5
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-25 09:28:54 +0530

    797

--- JULY30DAYSLEETCODECHALLENGE/AllpathFromSourceToTarget.py
+++ JULY30DAYSLEETCODECHALLENGE/AllpathFromSourceToTarget.py
@@ -1,3 +1,23 @@
 #797
 #rough page no 67
 
+def allpathSource(input):
+    global result
+    result = []
+    dfs([], 0)
+    return result
+def dfs(path, u):
+    if u == len(input) - 1:
+        result.append(path +[u])
+    else:
+        for v in input[u]:
+            dfs(path + [u], v)
+        
+
+
+
+
+
+input = [[1,2], [3], [3], []] 
+k = allpathSource(input)
+print(k)
