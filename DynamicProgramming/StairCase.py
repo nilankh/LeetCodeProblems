20516357e399d4f26f73b22d23f30922775c00e2
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-26 19:53:51 +0530

    done

--- DynamicProgramming/StairCase.py
+++ DynamicProgramming/StairCase.py
@@ -19,10 +19,20 @@ def helper(n, dp):
         return dp[n]
     dp[n] = helper(n - 3, dp) + helper(n - 2, dp) + helper(n - 1, dp)
     return dp[n]
+
+#iterative method
+def findStepI(n):
+    dp = [0 for i in range(n + 1)]
+    dp[0] = 1
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+    return dp[n]
 n=int(input())
 ##print(FindStep(n))
-print(findStepM(n))
-
+##print(findStepM(n))
+print(findStepI(n))
 
 
 
