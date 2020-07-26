087f4599b995be4b0f93c81e17057306d5a4f00f
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-26 11:00:22 +0530

    stair case

--- DynamicProgramming/StairCase.py
+++ DynamicProgramming/StairCase.py
@@ -3,9 +3,57 @@ def FindStep(n):
         return 1
     elif n==2:
         return 2
-    elif n==3:
-        return 4
     else:
         return FindStep(n-3)+FindStep(n-2)+FindStep(n-1)
+
+#memoization method
+def findStepM(n):
+    dp = [0 for i in range(n + 1)]
+    return helper(n, dp)
+def helper(n, dp):
+    if n == 0 or n == 1:
+        return 1
+    if n == 2:
+        return 2
+    if dp[n] > 0:
+        return dp[n]
+    dp[n] = helper(n - 3, dp) + helper(n - 2, dp) + helper(n - 1, dp)
+    return dp[n]
 n=int(input())
-print(FindStep(n))
+##print(FindStep(n))
+print(findStepM(n))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
