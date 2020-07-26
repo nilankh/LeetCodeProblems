043196338eb043da6ec1c88734c2e5e2350a5f7b
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-26 10:26:14 +0530

    done

--- DynamicProgramming/FibonaaciMemo.py
+++ DynamicProgramming/FibonaaciMemo.py
@@ -13,7 +13,33 @@ def fibb(n):
 n = int(input())
 ans = fibb(n)
 print(ans)'''
+#ye recursiin h
 
+def fibb(n, dp):
+    if n == 0 or n == 1:
+        return n
+
+    if dp[n - 1] == -1:
+        ans1 = fibb(n - 1, dp)
+        dp[n - 1] = ans1
+    else:
+        ans1 = dp[n - 1]
+
+    if dp[n - 2] == -1:
+        ans2 = fibb(n - 2, dp)
+        dp[n - 2] = ans2
+    else:
+        ans2 = dp[n - 2]
+        
+    myAns = ans1 + ans2
+    return myAns
+
+
+n = int(input())
+dp = [-1 for i in range(n + 1)]
+print(dp)
+ans = fibb(n, dp)
+print(ans)
 
 
 
