243bedb4c1deb9c6944140ba8da016c1efc9c992
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-26 08:52:43 +0530

    done 154

--- JULY30DAYSLEETCODECHALLENGE/FindMinimumInRotateSortedArray#154.py
+++ JULY30DAYSLEETCODECHALLENGE/FindMinimumInRotateSortedArray#154.py
@@ -4,10 +4,15 @@ def minRotate1(arr):
     return min(arr)
     
 
-
+def minRotate2(arr):
+    minNum = arr[0]
+    for i in range(1, len(arr)):
+        if arr[i] < minNum:
+            minNum = arr[i]
+    return minNum
 arr = [int(x) for x in input().split()]
-print(minRotate1(arr))
-
+#print(minRotate1(arr))
+print(minRotate2(arr))
 
 
 
