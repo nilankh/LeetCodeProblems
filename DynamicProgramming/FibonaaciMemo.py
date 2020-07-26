26bd830ac2dc7124ea512d06f243f116d4103668
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-26 10:26:31 +0530

    done

--- DynamicProgramming/FibonaaciMemo.py
+++ DynamicProgramming/FibonaaciMemo.py
@@ -41,6 +41,27 @@ print(dp)
 ans = fibb(n, dp)
 print(ans)
 
+#now we willl do this same problem with iterative method
+'''
+def fiboI(n):
+    dp = [0 for i in range(n + 1)]
+    dp[0] = 0
+    dp[1] = 1
+    i = 2
+    while i <= n:
+        dp[i] = dp[i - 1] + dp[i - 2]
+        i += 1
+    return dp[n]
+    
+
+
+
+n = int(input())
+ans = fiboI(n)
+print(ans)
+
+'''
+
 
 
 
