7f680e901706eb9991c083bca9016f9baf776723
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-26 20:08:23 +0530

    done with 70

--- DynamicProgramming/ClimbingStairs#70.py
+++ DynamicProgramming/ClimbingStairs#70.py
@@ -1,4 +1,5 @@
 #70
+#recursive approach
 def climbingStairs(n):
     if n == 0 or n == 1:
         return 1
@@ -7,10 +8,45 @@ def climbingStairs(n):
     else:
         return climbingStairs(n - 2) + climbingStairs(n - 1)
     
-
+#memoization
+def climbingStairsM(n):
+    dp = [0 for i in range( n + 1)]
+    return helperM(n, dp)
+def helperM(n, dp):
+    if n == 0:
+        return 1
+    if n < 0:
+        return 0
+    if dp[n] > 0:
+        return dp[n]
+    dp[n] = helperM(n - 1, dp) + helperM(n - 2, dp)
+    return dp[n]
 
 n = int(input())
-print(climbingStairs(n))
+#print(climbingStairs(n))
+print(climbingStairsM(n))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
