4f5abb93545505aa5ed1edfc51756ccd1b5a0782
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-27 20:53:13 +0530

    done 279

--- JULY30DAYSLEETCODECHALLENGE/Adddigits#258.py
+++ JULY30DAYSLEETCODECHALLENGE/Adddigits#258.py
@@ -10,11 +10,16 @@ def AddDigits(n):
             summ = 0
     return summ
         
-    
-    
 
+#Time O(1) Space O(1)
+def Add(n):
+    if(n == 0):
+        return 0
+    if n % 9 == 0:
+        return 9
+    else:
+        return n % 9
     
-
-
 n = int(input())
-print(AddDigits(n))
+#print(AddDigits(n))
+print(Add(n))
