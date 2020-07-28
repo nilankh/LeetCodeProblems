5312503c88bf9c116f36ff26aa45a5dd79f5d837
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-28 10:12:41 +0530

    done

--- DynamicProgramming/minSquaresToRepresentN.py
+++ DynamicProgramming/minSquaresToRepresentN.py
@@ -9,6 +9,7 @@ def minSquares(n):
         ans = min(ans, currAns)
     return ans
 
+#time complexity is nrootn.
 def minSquaresRM(n, dp):
     if n == 0:
         return 0
@@ -26,10 +27,25 @@ def minSquaresRM(n, dp):
         ans = min(ans, currAns)
     return ans
 
+#time complexity nrootn sp = O(N)
+def minSquaresI(n):
+    dp = [-1 for i in range(n + 1)]
+    dp[0] = 0
+    for i in range(1, n + 1):
+        ans = sys.maxsize
+        root = int(math.sqrt(i))
+        for j in range(1, root + 1):
+            currAns = 1 + dp[i - (j ** 2)]
+            ans = min(ans, currAns)
+        dp[i] = ans
+    return dp[n]
+        
+    
 n = int(input())
-dp = [-1 for i in range(n + 1)]
-ans = minSquaresRM(n, dp)
+#dp = [-1 for i in range(n + 1)]
+#ans = minSquaresRM(n, dp)
 ##ans = minSquares(n)
+ans = minSquaresI(n)
 print(ans)
 
 
