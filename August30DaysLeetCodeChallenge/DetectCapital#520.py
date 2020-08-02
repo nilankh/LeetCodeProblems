b2c1e0a65fa38a61b417b9e92f23361b8150c93c
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-02 08:30:21 +0530

    done 520

--- August30DaysLeetCodeChallenge/DetectCapital#520.py
+++ August30DaysLeetCodeChallenge/DetectCapital#520.py
@@ -35,12 +35,31 @@ def detectCapital(word):
     #if not matching
     return False
     
+##word = input()
+##print(detectCapital(word))
 
+def detectCapital2(word):
+    n = len(word)
+    if len(word) == 1:
+        return True
 
+    #Case 1: ALL CAPITAL
+    if word[0].isupper() and word[1].isupper():
+        for i in range(2, n):
+            if not word[i].isupper():
+                return False
 
+    #case 2 and case 3 (All not capital & all not capital except first)
+    else:
+        for i in range(1, n):
+            if word[i].isupper():
+                return False
+    #if pass one of the cases
+    return True
+    
 
 word = input()
-print(detectCapital(word))
+print(detectCapital2(word))
 
 
 
