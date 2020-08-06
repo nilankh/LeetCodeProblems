4ac5043ea4b0e3770eb1a1d9c6a0df8122877b65
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-06 18:09:10 +0530

    mcm

--- DynamicProgramming/MinimumCostPath.py
+++ DynamicProgramming/MinimumCostPath.py
@@ -1,27 +1,92 @@
 
 #first way recursion overlapping
+##import sys
+##def minCost(cost, i, j, n, m):
+##    
+##
+##    #special Case
+##    if i == n - 1 and j == m - 1:
+##        return cost[i][j]
+##    #Base Case
+##    if i >= n or j >= m:
+##        return sys.maxsize
+##
+##    ans1 = minCost(cost, i, j + 1, n, m)
+##    ans2 = minCost(cost, i + 1, j, n, m)
+##    ans3 = minCost(cost, i + 1, j + 1, n, m)
+##
+##    ans = cost[i][j] + min(ans1, ans2, ans3)
+##    return ans
+###cost=[[1,2],[3,4]]
+###cost = [[1, 2, 3], [4, 5, 9], [7, 3, 6]]
+##cost = [[1, 5, 11], [8, 13, 12], [2, 3, 7], [15, 16, 18]]
+##ans = minCost(cost, 0, 0, 4, 3)
+##print(ans)
+
+
+
+
+
+#second way memoization
 import sys
-def minCost(cost, i, j, n, m):
+def minCost(cost, i, j, n, m, dp):
     
 
     #special Case
     if i == n - 1 and j == m - 1:
         return cost[i][j]
+    
     #Base Case
     if i >= n or j >= m:
         return sys.maxsize
-
-    ans1 = minCost(cost, i, j + 1, n, m)
-    ans2 = minCost(cost, i + 1, j, n, m)
-    ans3 = minCost(cost, i + 1, j + 1, n, m)
+    
+    if dp[i][j + 1] == sys.maxsize:
+        ans1 = minCost(cost, i, j + 1, n, m, dp)
+        dp[i][j + 1] = ans1
+    else:
+        ans1 = dp[i][j + 1]
+        
+    if dp[i + 1][j] == sys.maxsize:
+        ans2 = minCost(cost, i + 1, j, n, m, dp)
+        dp[i + 1][j] = ans2
+    else:
+        ans2 = dp[i + 1][j]
+        
+    if dp[i + 1][j + 1] == sys.maxsize:
+        ans3 = minCost(cost, i + 1, j + 1, n, m, dp)
+        dp[i + 1][j + 1] = ans3
+    else:
+        ans3 = dp[i + 1][j + 1]
 
     ans = cost[i][j] + min(ans1, ans2, ans3)
     return ans
-#cost=[[1,2],[3,4]]
-#cost = [[1, 2, 3], [4, 5, 9], [7, 3, 6]]
+
 cost = [[1, 5, 11], [8, 13, 12], [2, 3, 7], [15, 16, 18]]
-ans = minCost(cost, 0, 0, 4, 3)
+n = 4
+m = 3
+dp = [[sys.maxsize for j in range(m + 1)] for i in range(n + 1)]
+
+ans = minCost(cost, 0, 0, 4, 3, dp)
 print(ans)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
