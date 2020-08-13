8c30502635e12385498f5596228536e068a4069d
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-13 20:49:08 +0530

    knapsack

--- DynamicProgramming/knapsack01.py
+++ DynamicProgramming/knapsack01.py
@@ -1,26 +1,86 @@
 #1st way recursion 
-def knapsack(W, val, wt, n, i):
+##def knapsack(W, val, wt, n, i):
+##
+##    #base case
+##    if i == n:
+##        return 0
+##
+##    if wt[i] > W:
+##        ans = knapsack(W, val, wt, n, i + 1)
+##
+##    else:
+##        #inclusion of ith ite,
+##        ans1 = val[i] + knapsack(W - wt[i], val, wt, n, i + 1)
+##
+##        #exclusion of the ith item
+##        ans2 = knapsack(W, val, wt, n, i + 1)       
+##        ans = max(ans1, ans2)
+##    return ans
+##
+##
+##val = [200, 300, 100]
+##wt = [20, 25, 30]
+##W = 50
+##n = len(val)
+##ans = knapsack(W, val, wt, n, 0)
+##print(ans)
 
-    #base case
-    if i == n:
+
+
+#2nd way
+#overlapping problems in Knapsack
+##recursion memoziation shared by student
+def knapsack(value,weight,w,i,n,dp):
+    if i==n:
         return 0
-
-    if wt[i] > W:
-        ans = knapsack(W, val, wt, n, i + 1)
-
+    
+    if weight[i]>w:
+        if dp[i+1][w]==-1:
+            ans=knapsack(value,weight,w,i+1,n,dp)
+            dp[i+1][w]=ans
+        else:
+            ans=dp[i+1][w]
     else:
-        #inclusion of ith ite,
-        ans1 = val[i] + knapsack(W - wt[i], val, wt, n, i + 1)
-
-        #exclusion of the ith item
-        ans2 = knapsack(W, val, wt, n, i + 1)       
-        ans = max(ans1, ans2)
+        if dp[i+1][w-weight[i]]==-1:
+            small_ans1=knapsack(value,weight,w-weight[i],i+1,n,dp)
+            ans1=value[i]+small_ans1
+            dp[i+1][w-weight[i]]=small_ans1
+        else:
+            ans1=dp[i+1][w-weight[i]]+value[i]
+        if dp[i+1][w]==-1:
+            ans2=knapsack(value,weight,w,i+1,n,dp)
+            dp[i+1][w]=ans2
+        else:
+            ans2=dp[i+1][w]
+        ans=max(ans1,ans2)
     return ans
+n=int(input())
+weight=[int(ele) for ele in input().split()]
+value=[int(ele1) for ele1 in input().split()]
+w=int(input())
+dp=[[-1 for j in range(w+1)]for i in range (n+1)]
+a=knapsack(value,weight,w,0,n,dp)
+print(a)
 
 
-val = [200, 300, 100]
-wt = [20, 25, 30]
-W = 50
-n = len(val)
-ans = knapsack(W, val, wt, n, 0)
-print(ans)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
