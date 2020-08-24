046d93346b39b79c273e4283ccf28283cd52e3a4
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-24 21:43:57 +0530

    #1025 memoization method

--- DynamicProgramming/DivisiorGame#1025.py
+++ DynamicProgramming/DivisiorGame#1025.py
@@ -1,15 +1,59 @@
 #page no 73-75
-def canWin(n):
+#time complexity is O(2^n)
+##def canWin(n):
+##    if n <= 1:
+##        return False
+##    for x in range(1,n):
+##        if(n % x == 0):
+##            if not canWin(n - x):
+##                return True
+##    return False
+##
+##n = int(input())
+##print(canWin(n))
+
+
+
+#Memoization Method
+#O(N^2)
+def canWinRM(n, cache):
     if n <= 1:
         return False
-    for x in range(1,n):
-        if(n % x == 0):
-            if not canWin(n - x):
+    if cache[n] is not None:
+        return cache[n]
+##    for x in range(1, n + 1):
+    for x in range(1, n//2 + 1):
+        if n % x == 0:
+            if not canWinRM(n - x, cache):
+                cache[n] = True
                 return True
+    cache[n] = False
     return False
-
 n = int(input())
-print(canWin(n))
+cache = [None for i in range(n + 1)]
+print(canWinRM(n, cache))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
