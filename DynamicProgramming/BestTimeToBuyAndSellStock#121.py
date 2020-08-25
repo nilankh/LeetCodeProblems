95b2f5b447ab52eea93441aad1b415da14d724b4
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-25 21:55:50 +0530

    #121 dp meethod

--- DynamicProgramming/BestTimeToBuyAndSellStock#121.py
+++ DynamicProgramming/BestTimeToBuyAndSellStock#121.py
@@ -1,22 +1,60 @@
 #81
 #121
+import sys
+##def maxProfit(price):
+##    if not price:
+##        return 0
+##    ans = 0
+##    mini = price[0]
+##    for i in range(1, len(price)):
+##        if price[i] < mini:
+##            mini = price[i]
+##        else:
+##            ans = max(ans, price[i] - mini)
+##    return ans
+##
+##price = [int(x) for x in input().split()]
+##print(maxProfit(price))
 
-
-
-def maxProfit(price):
-    if not price:
+def maxProfitR(price):
+    if price == None or len(price) == 0:
         return 0
-    ans = 0
-    mini = price[0]
-    for i in range(1, len(price)):
-        if price[i] < mini:
-            mini = price[i]
-        else:
-            ans = max(ans, price[i] - mini)
-    return ans
+    return getMaxProfit(price, 0, sys.maxsize, 0)
+
+def getMaxProfit(price, index, minStockPrice, maxDiff):
+    if len(price) == index:
+        return maxDiff
+    earn = price[index] - minStockPrice
+    maxDiff = max(earn, maxDiff)
+    minStockPrice = min(minStockPrice, price[index])
+    return getMaxProfit(price, index + 1, minStockPrice, maxDiff)
+
 
 price = [int(x) for x in input().split()]
-print(maxProfit(price))
+print(maxProfitR(price))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
