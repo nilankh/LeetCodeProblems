d57c49e912cb1dc80a2f026b6e8f8662dff3fbdd
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-26 16:45:52 +0530

    done with #392

--- DynamicProgramming/isSubsequence#392.py
+++ DynamicProgramming/isSubsequence#392.py
@@ -1,16 +1,30 @@
 #392
 #81 page
+##def isSubsequence(s,t):
+##    if len(s) == 0:
+##        return True
+##    if len(t) == 0:
+##        return False
+##    if(s[0] == t[0]):
+##        return isSubsequence(s[1:],t[1:])
+##
+##    return isSubsequence(s,t[1:])
+
+#two pointer method
+'''
 def isSubsequence(s,t):
     if len(s) == 0:
         return True
     if len(t) == 0:
         return False
-    if(s[0] == t[0]):
-        return isSubsequence(s[1:],t[1:])
-
-    return isSubsequence(s,t[1:])
-
-
+    s_idx = 0
+##    t_idx = 0
+    for i in range(len(t)):
+        if(t[i] == s[s_idx]):
+           s_idx += 1
+        if(s_idx == len(s)):
+           return True
+    return s_idx == len(s)'''
 
 s = input()
 t = input()
