51efe482338b63124a6069ce636fbc25f8620289
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-27 21:06:01 +0530

    #53

---
+++ DynamicProgramming/maximumSubArray#53.py
@@ -0,0 +1,32 @@
+#53
+#page83
+def maximumSubArray(arr):
+    total_sum = max_sum = arr[0]
+    for i in arr[1:]:
+        total_sum = max(i, total_sum+ i)
+        max_sum = max(max_sum, total_sum)
+    return max_sum
+        
+    
+
+
+arr = [int(x) for x in input().split()]
+print(maximumSubArray(arr))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
