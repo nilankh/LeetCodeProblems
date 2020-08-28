73f251596d4740dc7f8569d5503e345b458224dc
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-28 20:06:13 +0530

    recusion method done for house robber #198

--- DynamicProgramming/HouseRobber#198.py
+++ DynamicProgramming/HouseRobber#198.py
@@ -1,20 +1,33 @@
 #198
-import sys
-def houseRobber(arr):
-    maxAdd = -999999999999
-    i = 0
-    j = 1
-    summ = 0
-    summ2 = 0
-    while(i < len(arr)):
-        summ = summ + arr[i]
-        i += 2
-    print(summ)
-    while j < len(arr):
-        summ2 = summ2 + arr[j]
-        j += 2
-    return max(summ, summ2)
 
+#wrong approach 
+##import sys
+##def houseRobber(arr):
+##    maxAdd = -999999999999
+##    i = 0
+##    j = 1
+##    summ = 0
+##    summ2 = 0
+##    while(i < len(arr)):
+##        summ = summ + arr[i]
+##        i += 2
+##    print(summ)
+##    while j < len(arr):
+##        summ2 = summ2 + arr[j]
+##        j += 2
+##    return max(summ, summ2)
+
+#correct by recursion
+def houseRobber(arr):
+    return helper(arr, 0)
+def helper(arr, currentIndex):
+    if currentIndex >= len(arr):
+        return 0
+    stealCurrent = arr[currentIndex] + helper(arr, currentIndex + 2)
+    skipCurrent = helper(arr, currentIndex + 1)
+
+    return max(stealCurrent, skipCurrent)
+    
 
 
     
