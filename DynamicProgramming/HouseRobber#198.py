ccf7b835f009638ead997aa84b961d1a40183b79
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-28 20:55:40 +0530

    done with #198

--- DynamicProgramming/HouseRobber#198.py
+++ DynamicProgramming/HouseRobber#198.py
@@ -29,21 +29,43 @@
 ##    return max(stealCurrent, skipCurrent)
     
 
-def houseRobberMemo(arr):
-    dp = [0 for i in range(len(arr) + 1)]
-    return houseRobberMemoHelper(arr, 0, dp)
-def houseRobberMemoHelper(arr, currentIndex, dp):
-    if currentIndex >= len(arr):
+##def houseRobberMemo(arr):
+##    dp = [0 for i in range(len(arr) + 1)]
+##    return houseRobberMemoHelper(arr, 0, dp)
+##def houseRobberMemoHelper(arr, currentIndex, dp):
+##    if currentIndex >= len(arr):
+##        return 0
+##    if dp[currentIndex] != 0:
+##        return dp[currentIndex]
+##    
+##    stealCurrent = arr[currentIndex] + houseRobberMemoHelper(arr, currentIndex + 2, dp)
+##    skipCurrent = houseRobberMemoHelper(arr, currentIndex + 1, dp)
+##    return dp[currentIndex] + max(stealCurrent, skipCurrent)
+##    
+##arr = [int(l) for l in input().split()]
+##print(houseRobberMemo(arr))
+
+
+#iterative
+def houseRobI(arr):
+    if len(arr) == 0:
         return 0
-    if dp[currentIndex] != 0:
-        return dp[currentIndex]
+    if len(arr) == 1:
+        return arr[0]
+    if len(arr) == 2:
+        return max(arr[0], arr[1])
+
+    dp = [0 for i in range(len(arr) + 1)]
+    dp[0] = arr[0]
+    dp[1] = max(arr[0], arr[1])
+
+    for i in range(2, len(arr)):
+        dp[i] = max(arr[i] + dp[i - 2], dp[i - 1])
+    return dp[len(arr) - 1]
+        
+arr = [int(k) for k in input().split()]
+print(houseRobI(arr))
     
-    stealCurrent = arr[currentIndex] + houseRobberMemoHelper(arr, currentIndex + 2, dp)
-    skipCurrent = houseRobberMemoHelper(arr, currentIndex + 1, dp)
-    return dp[currentIndex] + max(stealCurrent, skipCurrent)
-    
-arr = [int(l) for l in input().split()]
-print(houseRobberMemo(arr))
 
 
 
