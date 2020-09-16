d4176c0289d01bc6d7c2698f7903d80e1ddd750c
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-09-16 22:55:58 +0530

    #421 done with naive approach

---
+++ September30days/#421MaximumXorTwoNumInArr.py
@@ -0,0 +1,25 @@
+
+
+def maximumXOR(arr):
+    maxi = 0
+    for i in range(len(arr) - 1):
+        for j in range(i + 1, len(arr)):
+            maxi = max(maxi, arr[i] ^ arr[j])
+    return maxi
+
+
+
+arr = [int(x) for x in input().split()]
+print(maximumXOR(arr))
+
+
+
+
+
+
+
+
+
+
+
+
