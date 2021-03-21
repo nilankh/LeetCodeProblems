7ebc9a74416cface112dcdfcb4add5ec2d4fe00b
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-03-21 12:40:52 +0530

    done

--- BinaryTree/BuildTreeUsingINorder&PreOrder.py
+++ BinaryTree/BuildTreeUsingINorder&PreOrder.py
@@ -54,6 +54,7 @@ def buildTreePreOrder(preOrder,inOrder):
     # rightPreOrder = preOrder[lenleftSubtree + 1:]
     
     
+    
     # Calling recursion on both left and right with finding preOrder of both left and right    
     leftChild=buildTreePreOrder(preOrder[1:left+1],leftIn)
     rightChild=buildTreePreOrder(preOrder[left+1:],rightIn)
